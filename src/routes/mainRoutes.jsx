a927822cad81ce72dd01d5ac5db7123bcfc87b7a
shovon mahmud <shovonrnbd@gmail.com>
2024-08-10 18:29:11 +0600

    add not found page

--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Main from "../layouts/main"
 import HomePage from "../pages"
+import NotFound from "../pages/notFound"
 // import ScrollToTop from "../hooks/StartTop"
 
 const MainRoutes = () => {
@@ -11,6 +12,12 @@ const MainRoutes = () => {
                 <Route element={<Main />}>
                     <Route path="/" element={<HomePage />} />
                 </Route>
+                <Route
+                    path="*"
+                    element={
+                        <NotFound />
+                    }
+                />
             </Routes>
         </BrowserRouter>
 
